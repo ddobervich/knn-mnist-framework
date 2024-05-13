bbff700c33d0fb2e0a154c1ad2ac819a79f6224a
David Dobervich <david.dobervich@gmail.com>
2024-05-13 09:50:27 -0700

    refactor to be slightly more memory efficient and also to load less than the full data set

--- src/Model/DataLoader.java
+++ src/Model/DataLoader.java
@@ -2,6 +2,9 @@ package Model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class DataLoader {
@@ -15,40 +18,17 @@ public class DataLoader {
         return s.replace("\r\n", "\n").replace('\r', '\n');
     }
 
-    public static String readFileAsString(String filepath) {
-        Scanner scanner = null;
+    public static List<String> readFileAsStrings(String filepath) {
         try {
-            scanner = new Scanner( new File(filepath) );
-            String text = scanner.useDelimiter("\\A").next();
-            return text;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (scanner != null)
-                scanner.close(); // Put this call in a finally block
+            return Files.readAllLines(Paths.get(filepath));
+        } catch (IOException e) {
+            System.err.println("-------------------------------------------------------");
+            System.err.println("There was an error reading your data file: " + filepath);
+            System.err.println("Check the file path!");
+            System.err.println("-------------------------------------------------------");
         }
-
-        System.err.println("-------------------------------------------------------");
-        System.err.println("There was an error reading your data file: " + filepath);
-        System.err.println("Check the file path!");
-        System.err.println("-------------------------------------------------------");
 
         return null;
-    }
-
-    public static List<DataPoint> loadHeartDiseaseData(String filepath) {
-        /* NOTE:  THIS IS ONLY FOR LOADING HEART DISEASE DATA; NOT FOR HAND-WRITTEN DIGIT RECOGNITION */
-        String data = normalizeLineBreaks(readFileAsString(filepath));
-        String[] lines = data.split("\n");
-
-        // create storage for data
-        ArrayList<DataPoint> dataset = new ArrayList<>();
-
-        for (int a = 0; a < lines.length; a++) {
-            // TODO: create Model.DataPoint objects here!
-        }
-
-        return dataset;
     }
 
     public static void splitDataIntoTrainingAndTest(List<DataPoint> allData, List<DataPoint> emptyTrainingList, List<DataPoint> emptyTestList, double percentTraining) {
@@ -62,22 +42,43 @@ public class DataLoader {
     }
 
     public static List<DataPoint> loadMNistData(String filepath) {
-        String data = normalizeLineBreaks(readFileAsString(filepath));
-        String[] lines = data.split("\n");
+        return loadMNistData(filepath, -1);
+    }
+
+    /***
+     * load data from an mnist csv file and return a List of DataPoint objects
+     * @param filepath
+     * @param num the number of examples to load (will be randomized).  -1 to load all.
+     * @return
+     */
+    public static List<DataPoint> loadMNistData(String filepath, int num) {
+        List<String> lines = readFileAsStrings(filepath);
+        Collections.shuffle(lines);
 
         // create storage for data
         ArrayList<DataPoint> dataset = new ArrayList<>();
 
-        for (String line : lines) {
+        if (num == -1) {
+            num = lines.size();
+        } else {
+            lines.subList(num, lines.size()).clear();
+        }
+
+        for (int i = 0; i < num; i++) {
+            String line = lines.get(i);
+            if (i % 1000 == 0) System.out.println("i:" + i);
+
             String[] values = line.split(",");
             String label = values[0];
+
             double[] featureVector = new double[values.length-1];
-            for (int i = 0; i < featureVector.length; i++) {
-                double grayValue = Double.parseDouble(values[i+1]);
-                featureVector[i] = 255 - grayValue;                     // color reverse so background is white
+            for (int j = 0; j < featureVector.length; j++) {
+                double grayValue = Double.parseDouble(values[j+1]);
+                featureVector[j] = 255 - grayValue;                     // color reverse so background is white
                                                                         // this will let our interactive classifier
                                                                         // run better.
             }
+
             DataPoint point = new DataPoint(label, featureVector);
             dataset.add(point);
         }
